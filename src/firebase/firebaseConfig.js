d8c313a01b5113444de7cbbefbc10591197ab8ff
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-09-28 09:52:50 +0530

    Firebase object initalized

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg2MevlQnvqYeAUh0OtYdWI0oyti2-jKw",
+  authDomain: "gform-clone.firebaseapp.com",
+  projectId: "gform-clone",
+  storageBucket: "gform-clone.appspot.com",
+  messagingSenderId: "533258869067",
+  appId: "1:533258869067:web:bdf71f48e72542432370ce",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
